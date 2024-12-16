46146528bf39a32b64422946cc4570fb7dd85035
NoNames <arthur30700@gmail.com>
2024-12-16 02:26:24 +0100

    Removed navic + Updated dracula theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -9,18 +9,18 @@ return {
   --     colorscheme = "pywal16",
   --   },
   -- },
-  {
-    "maxmx03/dracula.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
   -- {
-  --   "binhtran432k/dracula.nvim",
+  --   "maxmx03/dracula.nvim",
   --   lazy = false,
   --   priority = 1000,
   --   opts = {},
   -- },
+  {
+    "binhtran432k/dracula.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
   {
     "LazyVim/LazyVim",
     opts = {
