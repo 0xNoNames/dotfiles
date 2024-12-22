fbeb628c75e8c95eb16e395441e77209d37a396e
NoNames <arthur30700@gmail.com>
2024-12-23 00:54:05 +0100

    [neovim] Commented out python-lang

--- nvim/.config/nvim/lua/plugins/lang-python.lua
+++ nvim/.config/nvim/lua/plugins/lang-python.lua
@@ -1,53 +1,55 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = { ensure_installed = { "mypy", "pyright", "ruff" } },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        ruff = {
-          on_attach = function(client, _)
-            client.server_capabilities.hoverProvider = false
-          end,
-        },
-        pyright = {
-          capabilities = {
-            textDocument = {
-              publishDiagnostics = {
-                tagSupport = {
-                  valueSet = { 2 },
-                },
-              },
-            },
-          },
-          settings = {
-            pyright = {
-              disableOrganizeImports = true, -- Using Ruff
-            },
-            python = {
-              analysis = {
-                ignore = { "*" }, -- Using Ruff
-                typeCheckingMode = "off", -- Using mypy
-              },
-            },
-          },
-        },
-      },
-    },
-  },
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        python = { "mypy" },
-      },
-      linters = {
-        mypy = {},
-      },
-    },
-  },
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = { ensure_installed = { "mypy" } },
+  -- },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  -- ruff = {
+  --   on_attach = function(client, _)
+  --     client.server_capabilities.hoverProvider = false
+  --   end,
+  -- },
+  -- pyright = {
+  --   capabilities = {
+  --     textDocument = {
+  --       publishDiagnostics = {
+  --         tagSupport = {
+  --           valueSet = { 2 },
+  --         },
+  --       },
+  --     },
+  --   },
+  --   settings = {
+  --     pyright = {
+  --       disableOrganizeImports = true, -- Using Ruff
+  --     },
+  --     python = {
+  --       analysis = {
+  --         ignore = { "*" }, -- Using Ruff
+  --         typeCheckingMode = "off", -- Using mypy
+  --       },
+  --     },
+  --   },
+  -- },
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "mfussenegger/nvim-lint",
+  --   opts = {
+  --     linters_by_ft = {
+  --       python = { "mypy" },
+  --     },
+  --     linters = {
+  --       mypy = {},
+  --     },
+  --   },
+  -- },
+  --
+  -- Automatic format imports
   {
     "stevearc/conform.nvim",
     opts = {
