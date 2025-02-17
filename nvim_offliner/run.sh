80638e4d709e9a6da2c6e91b85bbf57acd96c7a9
0xNoNames <arthur30700@gmail.com>
2025-02-17 19:35:26 +0100

    Fixed run.sh from nvim_offliner

--- nvim_offliner/run.sh
+++ nvim_offliner/run.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-mkdir output
+mkdir -p output
 chmod 777 output
 docker build -t neovim-offliner:latest docker-image
 docker run -it --rm -v $(pwd)/output/:/home/user/output/ neovim-offliner:latest
