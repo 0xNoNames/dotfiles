101dbd7a6a94dc920d2ed691ec61c0eeeea2b5d2
0xNoNames <arthur30700@gmail.com>
2025-04-08 00:25:30 +0200

    Remove wezterm-motions

--- common/nvim/lua/plugins/wezterm-motions.lua
+++ common/nvim/lua/plugins/wezterm-motions.lua
@@ -1,53 +1,53 @@
 return {
-  "mrjones2014/smart-splits.nvim",
-  keys = {
-    {
-      "<C-A-h>",
-      function()
-        require("smart-splits").resize_left()
-      end,
-    },
-    {
-      "<C-A-j>",
-      function()
-        require("smart-splits").resize_down()
-      end,
-    },
-    {
-      "<C-A-k>",
-      function()
-        require("smart-splits").resize_up()
-      end,
-    },
-    {
-      "<C-A-l>",
-      function()
-        require("smart-splits").resize_right()
-      end,
-    },
-    {
-      "<C-C-S-h>",
-      function()
-        require("smart-splits").move_cursor_left()
-      end,
-    },
-    {
-      "<C-S-j>",
-      function()
-        require("smart-splits").move_cursor_down()
-      end,
-    },
-    {
-      "<C-S-k>",
-      function()
-        require("smart-splits").move_cursor_up()
-      end,
-    },
-    {
-      "<C-S-l>",
-      function()
-        require("smart-splits").move_cursor_right()
-      end,
-    },
-  },
+  -- "mrjones2014/smart-splits.nvim",
+  -- keys = {
+  --   {
+  --     "<C-A-h>",
+  --     function()
+  --       require("smart-splits").resize_left()
+  --     end,
+  --   },
+  --   {
+  --     "<C-A-j>",
+  --     function()
+  --       require("smart-splits").resize_down()
+  --     end,
+  --   },
+  --   {
+  --     "<C-A-k>",
+  --     function()
+  --       require("smart-splits").resize_up()
+  --     end,
+  --   },
+  --   {
+  --     "<C-A-l>",
+  --     function()
+  --       require("smart-splits").resize_right()
+  --     end,
+  --   },
+  --   {
+  --     "<C-C-S-h>",
+  --     function()
+  --       require("smart-splits").move_cursor_left()
+  --     end,
+  --   },
+  --   {
+  --     "<C-S-j>",
+  --     function()
+  --       require("smart-splits").move_cursor_down()
+  --     end,
+  --   },
+  --   {
+  --     "<C-S-k>",
+  --     function()
+  --       require("smart-splits").move_cursor_up()
+  --     end,
+  --   },
+  --   {
+  --     "<C-S-l>",
+  --     function()
+  --       require("smart-splits").move_cursor_right()
+  --     end,
+  --   },
+  -- },
 }
