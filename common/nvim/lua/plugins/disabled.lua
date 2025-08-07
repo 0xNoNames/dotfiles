fdc570aedf49284d5a65a8e85d56325f19e38c54
0xNoNames <arthur30700@gmail.com>
2025-08-07 04:37:11 +0200

    Disable some plugins

--- common/nvim/lua/plugins/disabled.lua
+++ common/nvim/lua/plugins/disabled.lua
@@ -1,11 +1,22 @@
 return {
+  { "folke/tokyonight.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
   { "echasnovski/mini.ai", enabled = false },
   { "echasnovski/mini.files", enabled = false },
-  { "catppuccin/nvim", enabled = false },
-  { "folke/tokyonight.nvim", enabled = false },
-  { "folke/flash.nvim", enabled = false },
-  -- { "folke/noice.nvim", enabled = false },
+  { "nvim-lualine/lualine.nvim", enabled = false },
+  -- { "akinsho/bufferline.nvim", enabled = false },
   { "ibhagwan/fzf-lua", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = { enabled = false },
+      animate = { enabled = false },
+      explorer = { enabled = false },
+      picker = { enabled = false },
+    },
+  },
 }
