b03ae1aa1145924920b9b292d5a65d7cc89a3b28
0xNoNames <arthur30700@gmail.com>
2025-08-07 03:17:36 +0200

    Remove unused plugins

--- common/nvim/lua/plugins/disabled.lua
+++ common/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,9 @@
 return {
   { "echasnovski/mini.pairs", enabled = false },
+  { "echasnovski/mini.ai", enabled = false },
+  { "echasnovski/mini.files", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
+  { "ibhagwan/fzf-lua", enabled = false },
 }
