9d9da4253736bc09cf8b55d094fdd295e178a13f
0xNoNames <arthur30700@gmail.com>
2025-08-15 13:40:31 +0200

    Re-add noice and lualine

--- common/nvim/lua/plugins/disabled.lua
+++ common/nvim/lua/plugins/disabled.lua
@@ -1,14 +1,14 @@
 return {
   -- { "akinsho/bufferline.nvim", enabled = false },
+  -- { "folke/noice.nvim", enabled = false },
+  -- { "nvim-lualine/lualine.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
   { "echasnovski/mini.ai", enabled = false },
   { "echasnovski/mini.files", enabled = false },
   { "echasnovski/mini.pairs", enabled = false },
   { "folke/flash.nvim", enabled = false },
-  { "folke/noice.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
   { "ibhagwan/fzf-lua", enabled = false },
-  { "nvim-lualine/lualine.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 
   {
