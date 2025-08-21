d2295781eda12648f8d265bdc865d85099fdabde
0xNoNames <0xNoNames@proton.me>
2025-08-21 21:00:43 +0200

    Build using podman

--- nvim_offliner/run.sh
+++ nvim_offliner/run.sh
@@ -4,6 +4,6 @@ set -ex
 
 mkdir -p output
 chmod 777 output
-docker build -t neovim-offliner:latest docker-image
-docker run -it --rm -v $(pwd)/output/:/home/user/output/ neovim-offliner:latest
-docker image rm neovim-offliner:latest
+podman build -t neovim-offliner:latest docker-image
+podman run -it --rm -v $(pwd)/output/:/home/user/output/ neovim-offliner:latest
+podman image rm neovim-offliner:latest
