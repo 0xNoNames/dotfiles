4c22d745b04ea5cc019c4bc975bbb7671c3ed561
0xNoNames <arthur30700@gmail.com>
2025-09-14 23:57:19 +0200

    nvim: Add transparency, italics and bold lualine

--- common/nvim/lua/plugins/colorscheme.lua
+++ common/nvim/lua/plugins/colorscheme.lua
@@ -3,21 +3,31 @@ return {
   --   "rebelot/kanagawa.nvim",
   --   lazy = true,
   -- },
+
   -- {
   --   "rose-pine/neovim",
   --   name = "rose-pine",
   --   lazy = true,
   -- },
+
   {
     "binhtran432k/dracula.nvim",
     lazy = false,
     opts = {
-      -- style = "soft",
-      on_colors = function(colors)
-        colors.bg = "#000000"
-      end,
+      transparent = true,
+      lualine_bold = true,
+      functions = { italic = true },
+      variables = { italic = true },
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      -- on_colors = function(colors)
+      --   colors.bg = "#000000"
+      -- end,
     },
   },
+
   {
     "LazyVim/LazyVim",
     opts = {
