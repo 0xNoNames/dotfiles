5a4ede09bbeb3212aa30b4eb5030675c2e1aebe0
0xNoNames <arthur30700@gmail.com>
2025-09-14 23:51:37 +0200

    nvim: Disable fff

--- common/nvim/lua/plugins/fff.lua
+++ common/nvim/lua/plugins/fff.lua
@@ -1,18 +1,18 @@
 return {
-  "dmtrKovalenko/fff.nvim",
-  build = "cargo build --release",
-  keys = {
-    {
-      "<leader><leader>",
-      function()
-        require("fff").find_files()
-      end,
-      desc = "Open file picker",
-    },
-  },
-  opts = {
-    keymaps = {
-      close = "<C-c>",
-    },
-  },
+  --   "dmtrKovalenko/fff.nvim",
+  --   build = "cargo build --release",
+  --   keys = {
+  --     {
+  --       "<leader><leader>",
+  --       function()
+  --         require("fff").find_files()
+  --       end,
+  --       desc = "Open file picker",
+  --     },
+  --   },
+  --   opts = {
+  --     keymaps = {
+  --       close = "<C-c>",
+  --     },
+  --   },
 }
