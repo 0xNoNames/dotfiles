0870dd45aeb3bb94553366d128dfc70e54ec9129
0xNoNames <arthur30700@gmail.com>
2025-09-15 00:38:24 +0200

    nvim: Better transparency and add colorschemes

--- common/nvim/lua/plugins/colorscheme.lua
+++ common/nvim/lua/plugins/colorscheme.lua
@@ -10,20 +10,46 @@ return {
   --   lazy = true,
   -- },
 
+  -- {
+  --   "nyoom-engineering/oxocarbon.nvim",
+  -- },
+
+  -- {
+  --   "EdenEast/nightfox.nvim", -- carbonfox
+  --   lazy = false,
+  --   opts = {
+  --     options = {
+  --       transparent = true,
+  --       styles = {
+  --         comments = "italic",
+  --         functions = "italic",
+  --         variables = "italic",
+  --       },
+  --     },
+  --   },
+  -- },
+
   {
     "binhtran432k/dracula.nvim",
     lazy = false,
     opts = {
-      transparent = true,
       lualine_bold = true,
       functions = { italic = true },
       variables = { italic = true },
+      comments = { italic = true },
       styles = {
         sidebars = "transparent",
         floats = "transparent",
       },
-      -- on_colors = function(colors)
-      --   colors.bg = "#000000"
+      transparent = true,
+      on_colors = function(colors)
+        colors.bg = "#000000"
+      end,
+      -- on_highlights = function(hl, c)
+      --   hl.DiagnosticVirtualTextError = { bg = c.none, fg = c.error }
+      --   hl.DiagnosticVirtualTextWarn = { bg = c.none, fg = c.warning }
+      --   hl.DiagnosticVirtualTextInfo = { bg = c.none, fg = c.info }
+      --   hl.DiagnosticVirtualTextHint = { bg = c.none, fg = c.hint }
       -- end,
     },
   },
